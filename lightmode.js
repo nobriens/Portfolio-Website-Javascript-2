cfc42a479a8e07412986a8ba50beb2a1ef851b3d
nobriens <nobriens@tcd.ie>
2026-01-12 23:59:47 +0000

    Update lightmode.js

--- lightmode.js
+++ lightmode.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("theme-toggle");
-  if (!toggle) return;
+document.addEventListener("DOMContentLoaded", () => { // Waits until the page has fully loaded before running the script
+  const toggle = document.getElementById("theme-toggle");  // Select the light and dark mode toggle checkbox
+  if (!toggle) return; // If there is no toggle on the page it will stop the script
 
-  // Load saved preference
+  // Loads devices saved preference if visited before and applys it
   const savedTheme = localStorage.getItem("theme");
 
   if (savedTheme === "light") {
@@ -10,10 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
     toggle.checked = true;
   }
 
-  toggle.addEventListener("change", () => {
+  toggle.addEventListener("change", () => { // Listens for when the user changes the toggle
     const isLight = toggle.checked;
 
-    document.body.classList.toggle("light", isLight);
-    localStorage.setItem("theme", isLight ? "light" : "dark");
+    document.body.classList.toggle("light", isLight); // Adds or removes the light class on the body
+    localStorage.setItem("theme", isLight ? "light" : "dark"); // Saves the user’s preference so it stays across all pages
   });
+
 });
